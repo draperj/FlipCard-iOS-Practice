7858c026ffcba7b0fa06ced07a53e35f78a23457
Joshua Draper <draperj@NYC-P011356-M.local>
2018-03-28 17:18:23 -0400

    Changes for lecture 8

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -31,104 +31,96 @@ class ViewController: UIViewController {
             cardView.rank = card.rank.order
             cardView.suit = card.suit.rawValue
             cardView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(flipCard(_:))))
-            //            cardBehavior.addItem(cardView)
+            cardBehavior.addItem(cardView)
         }
     }
+    
+    private var faceUpCardViews: [PlayingCardView] {
+        return cardViews.filter { $0.isFaceUp && !$0.isHidden && $0.transform != CGAffineTransform.identity.scaledBy(x: 3.0, y: 3.0) && $0.alpha == 1 }
+    }
+    
+    private var faceUpCardViewsMatch: Bool {
+        return faceUpCardViews.count == 2 &&
+            faceUpCardViews[0].rank == faceUpCardViews[1].rank &&
+            faceUpCardViews[0].suit == faceUpCardViews[1].suit
+    }
+    
+    var lastChosenCardView: PlayingCardView?
     
     @objc func flipCard(_ recognizer: UITapGestureRecognizer) {
         switch recognizer.state {
-        case .ended :
-            if let chosenCardView = recognizer.view as? PlayingCardView {
-                chosenCardView.isFaceUp = !chosenCardView.isFaceUp
+        case .ended:
+            if let chosenCardView = recognizer.view as? PlayingCardView, faceUpCardViews.count < 2 {
+                lastChosenCardView = chosenCardView
+                cardBehavior.removeItem(chosenCardView)
+                UIView.transition(
+                    with: chosenCardView,
+                    duration: 0.5,
+                    options: [.transitionFlipFromLeft],
+                    animations: {
+                        chosenCardView.isFaceUp = !chosenCardView.isFaceUp
+                },
+                    completion: { finished in
+                        let cardsToAnimate = self.faceUpCardViews
+                        if self.faceUpCardViewsMatch {
+                            UIViewPropertyAnimator.runningPropertyAnimator(
+                                withDuration: 0.6,
+                                delay: 0,
+                                options: [],
+                                animations: {
+                                    cardsToAnimate.forEach {
+                                        $0.transform = CGAffineTransform.identity.scaledBy(x: 3.0, y: 3.0)
+                                    }
+                            },
+                                completion: { position in
+                                    UIViewPropertyAnimator.runningPropertyAnimator(
+                                        withDuration: 0.75,
+                                        delay: 0,
+                                        options: [],
+                                        animations: {
+                                            cardsToAnimate.forEach {
+                                                $0.transform = CGAffineTransform.identity.scaledBy(x: 0.1, y: 0.1)
+                                                $0.alpha = 0
+                                            }
+                                    },
+                                        completion: { position in
+                                            cardsToAnimate.forEach {
+                                                $0.isHidden = true
+                                                $0.alpha = 1
+                                                $0.transform = .identity
+                                            }
+                                    }
+                                    )
+                            }
+                            )
+                        } else if cardsToAnimate.count == 2 {
+                            if chosenCardView == self.lastChosenCardView {
+                                cardsToAnimate.forEach { cardView in
+                                    UIView.transition(
+                                        with: cardView,
+                                        duration: 0.5,
+                                        options: [.transitionFlipFromLeft],
+                                        animations: {
+                                            cardView.isFaceUp = false
+                                    },
+                                        completion: { finished in
+                                            self.cardBehavior.addItem(cardView)
+                                    }
+                                    )
+                                }
+                            }
+                        } else {
+                            if !chosenCardView.isFaceUp {
+                                self.cardBehavior.addItem(chosenCardView)
+                            }
+                        }
+                }
+                )
             }
-        default: break;
+        default:
+            break
         }
     }
-    
-    //    private var faceUpCardViews: [PlayingCardView] {
-    //        return cardViews.filter { $0.isFaceUp && !$0.isHidden && $0.transform != CGAffineTransform.identity.scaledBy(x: 3.0, y: 3.0) && $0.alpha == 1 }
-    //    }
-    //
-    //    private var faceUpCardViewsMatch: Bool {
-    //        return faceUpCardViews.count == 2 &&
-    //        faceUpCardViews[0].rank == faceUpCardViews[1].rank &&
-    //        faceUpCardViews[0].suit == faceUpCardViews[1].suit
-    //    }
-    //
-    //    var lastChosenCardView: PlayingCardView?
-    //
-    //    @objc func flipCard(_ recognizer: UITapGestureRecognizer) {
-    //        switch recognizer.state {
-    //        case .ended:
-    //            if let chosenCardView = recognizer.view as? PlayingCardView, faceUpCardViews.count < 2 {
-    //                lastChosenCardView = chosenCardView
-    //                cardBehavior.removeItem(chosenCardView)
-    //                UIView.transition(
-    //                    with: chosenCardView,
-    //                    duration: 0.5,
-    //                    options: [.transitionFlipFromLeft],
-    //                    animations: {
-    //                        chosenCardView.isFaceUp = !chosenCardView.isFaceUp
-    //                    },
-    //                    completion: { finished in
-    //                        let cardsToAnimate = self.faceUpCardViews
-    //                        if self.faceUpCardViewsMatch {
-    //                            UIViewPropertyAnimator.runningPropertyAnimator(
-    //                                withDuration: 0.6,
-    //                                delay: 0,
-    //                                options: [],
-    //                                animations: {
-    //                                    cardsToAnimate.forEach {
-    //                                        $0.transform = CGAffineTransform.identity.scaledBy(x: 3.0, y: 3.0)
-    //                                    }
-    //                                },
-    //                                completion: { position in
-    //                                    UIViewPropertyAnimator.runningPropertyAnimator(
-    //                                        withDuration: 0.75,
-    //                                        delay: 0,
-    //                                        options: [],
-    //                                        animations: {
-    //                                            cardsToAnimate.forEach {
-    //                                                $0.transform = CGAffineTransform.identity.scaledBy(x: 0.1, y: 0.1)
-    //                                                $0.alpha = 0
-    //                                            }
-    //                                        },
-    //                                        completion: { position in
-    //                                            cardsToAnimate.forEach {
-    //                                                $0.isHidden = true
-    //                                                $0.alpha = 1
-    //                                                $0.transform = .identity
-    //                                            }
-    //                                        }
-    //                                    )
-    //                                }
-    //                            )
-    //                        } else if cardsToAnimate.count == 2 {
-    //                            if chosenCardView == self.lastChosenCardView {
-    //                                cardsToAnimate.forEach { cardView in
-    //                                    UIView.transition(
-    //                                        with: cardView,
-    //                                        duration: 0.5,
-    //                                        options: [.transitionFlipFromLeft],
-    //                                        animations: {
-    //                                            cardView.isFaceUp = false
-    //                                        },
-    //                                        completion: { finished in
-    //                                            self.cardBehavior.addItem(cardView)
-    //                                        }
-    //                                    )
-    //                                }
-    //                            }
-    //                        } else {
-    //                            if !chosenCardView.isFaceUp {
-    //                                self.cardBehavior.addItem(chosenCardView)
-    //                            }
-    //                        }
-    //                    }
-    //                )
-    //            }
-    //        default:
-    //            break
-    //        }
-    //    }
 }
+
+
